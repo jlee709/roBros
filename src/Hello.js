d09384b079e33bb768d80ccdf5f45bf7600ff247
Justin Lee <leejwg.3@gmail.com>
2018-06-21 21:27:40 -0230

    final starter testing with starter component

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Hello extends Component {
+  render() {
+    return <h1>Hello World I am RoBro!</h1>;
+  }
+}
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Hello from "./Hello";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<h1>Hello Me tester</h1>, document.getElementById("root"));
+ReactDOM.render(<Hello />, document.getElementById("root"));
 registerServiceWorker();
