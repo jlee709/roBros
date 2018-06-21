1719869054a4de053d4ed6b220bc6089658f7d2d
Justin Lee <leejwg.3@gmail.com>
2018-06-21 21:14:22 -0230

    test header

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<h1>Hello Me</h1>, document.getElementById("root"));
+ReactDOM.render(<h1>Hello Me tester</h1>, document.getElementById("root"));
 registerServiceWorker();
