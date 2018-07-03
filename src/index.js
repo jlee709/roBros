b0802d4b78289da901dcd9b2011f72256ef68de3
Justin Lee <leejwg.3@gmail.com>
2018-07-03 00:38:22 -0230

    made proper file structure, moved around code slightly, refactoring++

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
 
-import { robros } from "./robros";
-import App from "./App";
-import "./App.css";
+// import { robros } from "./robros";
+import App from "./containers/App";
+import "./containers/App.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
