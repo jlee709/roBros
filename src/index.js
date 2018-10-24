a63ba8f44a3e9f7a31336f6700fb9fb6461f4cd0
Justin Lee <leejwg.3@gmail.com>
2018-10-24 18:45:18 +0900

    removed logger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
-// import { createLogger } from "redux-logger";
+import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
@@ -11,8 +11,6 @@ import { searchRobros, requestRobros } from "./reducers/searchRobros.js";
 // import { robros } from "./robros";
 import App from "./containers/App";
 import "./containers/App.css";
-
-// const logger = createLogger();
 
 const rootReducer = combineReducers({ searchRobros, requestRobros });
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
